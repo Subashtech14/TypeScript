491d17383d302ada827d7263e3a9093ed9d87d59
Subash <Subashtech14@gmail.com>
2023-09-17 13:48:16 +0530

    Functions and arrow functions and return type in typescript

--- 01-Intro/myfunctions.ts
+++ 01-Intro/myfunctions.ts
@@ -14,3 +14,31 @@ function loginUser(name:string,email:string,isPaid:boolean=false){
     return {name,email};
 }
 console.log(loginUser("Subash","subash@me.com",true));
+
+// function getValue(myVal:number):boolean{
+//     if(myVal > 5){
+//         return true;
+//     }
+//     return "200 OK";
+// }
+
+const getHello=(s:string):string=>{
+return "hi";
+}
+
+const heros=["thor","ironman","spiderman"]
+// const heros=[1,2,3]
+let user=heros.map((heros:string)=>{
+    return `hero is ${heros}`;
+})
+console.log(user);
+
+function consoleError(errmsg:string):void{
+    console.log(errmsg);
+}
+//never it is used for a function that never returns a value and that fucntion can't be reached
+function handleError(errmsg:string):never{
+    throw new Error(errmsg);
+}
+
+
