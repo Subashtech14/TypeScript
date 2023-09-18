126dce0e1449e19d074cf77577e07ae3e3b38a56
Subash <Subashtech14@gmail.com>
2023-09-18 07:53:11 +0530

    my Enum

---
+++ 01-Intro/myEnums.ts
@@ -0,0 +1,10 @@
+enum SeatChoice {
+  Window="10",
+  Middle=1,
+  Aisle,
+  None,
+}
+//we can change the value of the variable inside enum
+//the variable value will automatically calculated
+let hcSeat= SeatChoice.Aisle;
+console.log(hcSeat);
